999e37b825d54b90155fc0b5234f7c3c3bfa9bff
perfectionist7 <96302137+perfectionist7@users.noreply.github.com>
2023-03-06 09:31:10 +0530

    Add files via upload

---
+++ AL Lab Codes/lab-3/lab2_4.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+void Transpose(int a[100][100], int v)
+{
+    int b[100][100];
+    for (int i = 1; i <= v; i++)
+    {
+        for (int j = 1; j <= v; j++)
+        {
+            b[i][j] = a[j][i];
+        }
+    }
+
+    for (int i = 1; i <= v; i++)
+    {
+        for (int j = 1; j <= v; j++)
+        {
+            cout << b[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int v,e;
+    cout<<"Enter the vertices"<<endl;
+    cin>>v;
+    cout<<"Enter the edges"<<endl;
+    cin>>e;
+    int a[100][100];
+    int p,q;
+    for(int i=0;i<e;i++)
+    {
+        cout<<"Enter the source"<<endl;
+        cin>>p;
+        cout<<"Enter the destination"<<endl;
+        cin>>q;
+        a[p][q]=1;
+    }
+    cout << "Transpose of the given matrix: " << endl;
+    Transpose(a,p);
+}
