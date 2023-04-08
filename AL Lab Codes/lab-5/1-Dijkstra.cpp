fa780b6901f7a9c59c42229b95cbc95a4788fe20
Ayush Khandelwal <akhandelwal2003@gmail.com>
2023-04-08 08:01:26 +0530

    djikstra updated

--- AL Lab Codes/lab-5/1-Dijkstra.cpp
+++ AL Lab Codes/lab-5/1-Dijkstra.cpp
@@ -1,121 +1,63 @@
-// Program to implement Dijkstra Algorithm
 #include <iostream>
-#include <bits/stdc++.h>
-#include <algorithm>
+#include <stdio.h>
 using namespace std;
-const int inf = 9999;
 
-class Graph
+int miniDist(int distance[], bool Tset[]) // finding minimum distance
 {
-private:
-    int **adj;
-    int n;
+    int minimum = INT_MAX, ind;
 
-public:
-    Graph(int numb)
+    for (int k = 0; k < 6; k++)
     {
-        n = numb + 1;
-        adj = new int *[n];
-        for (int i = 0; i < n; ++i)
-            adj[i] = new int[n];
-        for (int i = 0; i < n; ++i)
+        if (Tset[k] == false && distance[k] <= minimum)
         {
-            for (int j = 0; j < n; j++)
-                adj[i][j] = inf;
-            adj[i][i] = 0;
+            minimum = distance[k];
+            ind = k;
         }
     }
-    void addVertex(int src, int dest, int cost)
+    return ind;
+}
+
+void DijkstraAlgo(int graph[6][6], int src) // adjacency matrix
+{
+    int distance[6]; // // array to calculate the minimum distance for each node
+    bool Tset[6];    // boolean array to mark visited and unvisited for each node
+
+    for (int k = 0; k < 6; k++)
     {
-        adj[src][dest] = cost;
+        distance[k] = INT_MAX;
+        Tset[k] = false;
     }
 
-    void dijkstra(int src)
+    distance[src] = 0; // Source vertex distance is set 0
+
+    for (int k = 0; k < 6; k++)
     {
-        int cost[n];
-        int pre[n] = {0};
-        bool visited[n] = {false};
-
-        // Intializing priority queue using lambda function
-        auto compare =
-            [=](int lhs, int rhs)
+        int m = miniDist(distance, Tset);
+        Tset[m] = true;
+        for (int k = 0; k < 6; k++)
         {
-            return adj[src][lhs] > adj[src][rhs];
-        };
-        priority_queue<int, vector<int>, decltype(compare)> list(compare);
-
-        for (int i = 1; i < n; i++)
-        {
-            cost[i] = adj[src][i]; // Intialize Predecessor and Cost array
-            if (cost[i] != inf && cost[i] != 0)
-            {
-                pre[i] = src;
-                list.push(i);
-                visited[i] = true;
-            }
+            // updating the distance of neighbouring vertex
+            if (!Tset[k] && graph[m][k] && distance[m] != INT_MAX && distance[m] + graph[m][k] < distance[k])
+                distance[k] = distance[m] + graph[m][k];
         }
-
-        while (!list.empty())
-        {
-            int i = list.top();
-            list.pop();
-            for (int j = 1; j < n; j++)
-            {
-                int temp = min(cost[j], cost[i] + adj[i][j]);
-                if (temp != cost[j])
-                {
-                    cost[j] = temp;
-                    pre[j] = i;
-                    if (!visited[j])
-                    {
-                        list.push(j);
-                        visited[j] = true;
-                    }
-                }
-            }
-        }
-
-        cout << "Node:        ";
-        for (int i = 1; i < n; i++)
-            cout << i << " ";
-        cout << endl
-             << "Cost:        ";
-        for (int i = 1; i < n; i++)
-        {
-            if (cost[i] == 9999)
-            {
-                cout << 0 << " ";
-            }
-            else
-            {
-                cout << cost[i] << " ";
-            }
-        }
-        cout << endl
-             << "Predecessor: ";
-        for (int i = 1; i < n; i++)
-            cout << pre[i] << " ";
     }
-};
+    cout << "Vertex\t\tDistance from source vertex" << endl;
+    for (int k = 0; k < 6; k++)
+    {
+        char str = 65 + k;
+        cout << str << "\t\t\t" << distance[k] << endl;
+    }
+}
 
 int main()
 {
-    int n;
-    cout << "Enter the number of nodes: ";
-    cin >> n;
-    Graph G(n);
-    cout << "Enter the number of edges: ";
-    int e;
-    cin >> e;
-    cout << "Enter edges and cost: " << endl;
-    int a, b, cost;
-    while (e)
-    {
-        cin >> a >> b >> cost;
-        G.addVertex(a, b, cost);
-        e--;
-    }
-    cout << "Enter the source: ";
-    cin >> n;
-    G.dijkstra(n);
+    int graph[6][6] = {
+        {0, 1, 2, 0, 0, 0},
+        {1, 0, 0, 5, 1, 0},
+        {2, 0, 0, 2, 3, 0},
+        {0, 5, 2, 0, 2, 2},
+        {0, 1, 3, 2, 0, 1},
+        {0, 0, 0, 2, 1, 0}};
+    DijkstraAlgo(graph, 0);
+    return 0;
 }
